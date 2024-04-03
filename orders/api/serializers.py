215cdb352b3649ea0ba77930ef5aee69e04f6d23
Nikolay Abarinov <n.abarinov@yandex.ru>
2024-04-03 13:18:48 +0300

    add orders views

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import User, Contact
+
+from .models import User, Contact, OrderItem, Order, Shop, Category, Product, ProductInfo, ProductParameter
 
 
 class ContactSerializer(serializers.ModelSerializer):
@@ -18,4 +19,70 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts')
+        read_only_fields = ('id',)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'product_info', 'quantity', 'order',)
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'order': {'write_only': True}
+        }
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ('id', 'name', 'state')
+        read_only_fields = ('id',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        fields = ('name', 'category',)
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductParameter
+        fields = ('parameter', 'value',)
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = ('id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
+        read_only_fields = ('id',)
+
+
+class OrderItemCreateSerializer(OrderItemSerializer):
+    product_info = ProductInfoSerializer(read_only=True)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
+
+    total_sum = serializers.IntegerField()
+    contact = ContactSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'ordered_items', 'state', 'dt', 'total_sum', 'contact',)
         read_only_fields = ('id',)
--- orders/api/urls.py
+++ orders/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
-from .views import RegisterAccount, LoginAccount, AccountDetails, ContactView, ConfirmAccount
+from .views import RegisterAccount, LoginAccount, AccountDetails, ContactView, ConfirmAccount, PartnerOrders, OrderView, BasketView
 
 app_name = 'api'
 
@@ -13,4 +13,7 @@ urlpatterns = [
     path('user/login', LoginAccount.as_view(), name='user-login'),
     path('user/password_reset', reset_password_request_token, name='password-reset'),
     path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'),
+    path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),
+    path('order', OrderView.as_view(), name='order'),
+    path('basket', BasketView.as_view(), name='basket'),
 ]
